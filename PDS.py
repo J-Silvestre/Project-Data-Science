0e4b37b4fd4fd7c395c70ad000bbe2e5b7b19508
J-Silvestre <90836876+J-Silvestre@users.noreply.github.com>
2022-03-24 17:29:33 +0000

    Update PDS.py

--- PDS.py
+++ PDS.py
@@ -13,4 +13,16 @@ Created on Sun Feb 20 13:32:53 2022
 # Just keeping up the hope it will finally work!!
 
 
-## NOW
+# What are the factors that make an airbnb accomodation more attractive?
+# External Data: Crime-Rate per municipality (Pordata)
+# External Data: Weather per municipality (Openweather)
+# External Data: Proximity to Lisbon City Centre, Proximity to Coast (ourselves)
+
+# Measure attractiveness through the number of days an appartment is rented
+
+# Extra: Logistic Regression to predict if our appartment would be rented given a set of regressors
+
+
+#https://zonamentopf.portaldasfinancas.gov.pt/simulador/default.jsp
+
+
