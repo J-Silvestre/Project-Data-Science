c951f8e99f2f7dc94bca513e4129c6582222b679
Rodilas <rodrigo.club13@gmail.com>
2022-03-24 14:11:23 +0000

    Update PDS.py

--- PDS.py
+++ PDS.py
@@ -6,3 +6,9 @@ Created on Sun Feb 20 13:32:53 2022
 """
 
 # hello joao
+
+#rodrigo
+import pandas as pd
+data = pd.read_csv("C:\\Users\\rodri\\Downloads\\calendar.csv.gz", compression='gzip',
+                   error_bad_lines=False)
+print(data.tail)
