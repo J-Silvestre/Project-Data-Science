4a4f5909c14046f3bea495d626ba70fe3c9ab0d4
J-Silvestre <90836876+J-Silvestre@users.noreply.github.com>
2022-03-24 14:29:37 +0000

    Update PDS.py

--- PDS.py
+++ PDS.py
@@ -5,6 +5,7 @@ Created on Sun Feb 20 13:32:53 2022
 @author: joaod
 """
 
+<<<<<<< HEAD
 # hello joao
 
 #rodrigo
@@ -12,3 +13,6 @@ import pandas as pd
 data = pd.read_csv("C:\\Users\\rodri\\Downloads\\calendar.csv.gz", compression='gzip',
                    error_bad_lines=False)
 print(data.tail)
+=======
+# hello joao
+>>>>>>> parent of e5fa365 (Delete PDS.py)
