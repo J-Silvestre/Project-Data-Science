c40a17723f594fe8204479ba836631d849f8f12e
J-Silvestre <90836876+J-Silvestre@users.noreply.github.com>
2022-04-06 01:21:59 +0100

    pop_density

--- neighbourhoods_update/neighbourhoods_update.py
+++ neighbourhoods_update/neighbourhoods_update.py
@@ -12,9 +12,12 @@ df_neigh = pd.read_csv("Neighbourhoods.csv", sep=",")
 
 df_purch = pd.read_excel("Purchase_power_pc.xlsx",sheet_name="Sheet1")
 df_crime = pd.read_excel("Crime_rate_pc.xlsx",sheet_name="Sheet1")
+df_pop = pd.read_excel("Population_density.xlsx", sheet_name="Sheet1")
 
-df_neigh2 = pd.merge(df_neigh, df_purch, how="inner", on="neighbourhood_group")
-df_neigh3 = pd.merge(df_neigh2, df_crime, how="inner", on="neighbourhood_group")
+
+df_neigh1 = pd.merge(df_neigh, df_purch, how="inner", on="neighbourhood_group")
+df_neigh2 = pd.merge(df_neigh1, df_crime, how="inner", on="neighbourhood_group")
+df_neigh3 = pd.merge(df_neigh2, df_pop, how="inner", on="neighbourhood_group")
 
 
 df_neigh3.to_csv("neighbourhoods_update.csv", sep=",")
