f294f682571824f2f387cdee778b5b8a01334582
Rodilas <rodrigo.club13@gmail.com>
2022-04-12 22:02:03 +0100

    Update Reviews.py

--- sentiment analysis/Reviews.py
+++ sentiment analysis/Reviews.py
@@ -72,6 +72,7 @@ stopwords.fileids()
 
 
 # Do a loop for all the languages to improve?
+#This code takes a lot of time to run
 stop_wordsEn = stopwords.words("english")
 stop_wordsGer = stopwords.words("german")
 stop_wordsFr = stopwords.words("french")
@@ -80,7 +81,7 @@ stop_wordsEs = stopwords.words("spanish")
 stop_wordsGr = stopwords.words("greek")
 
 
-
+#removing stop words for each language
 reviews["w/o_stopwords"]= reviews["punctuation"].apply(lambda x: " ".join(word for word in x.split() if word not in stop_wordsEn))
 reviews["w/o_stopwords"]= reviews["w/o_stopwords"].apply(lambda x: " ".join(word for word in x.split() if word not in stop_wordsGer))
 reviews["w/o_stopwords"]= reviews["w/o_stopwords"].apply(lambda x: " ".join(word for word in x.split() if word not in stop_wordsFr))
@@ -108,11 +109,72 @@ reviews["stop_words_rate"] = reviews["stop_word_count"] /reviews["punctuation"].
 #analyze this shit later, erros spotted due to language and other stuff 
 reviews.sort_values(by="stop_words_rate").head(30)
 
+#not working because of the empty values i think
+"""
+def comment_average(x):
+    words = x.split()
+    return sum(len(word) for word in words)/ len(words)
+"""
+#average length of words per comment, does the same thing as the function above
+reviews["avg_len_word"]=reviews["chars_count"]/reviews["word_count"]
+
+#nr of stop words per comment
+reviews["stop_word_count"] = reviews["punctuation"].apply(lambda x: len(x.split())) - reviews["word_count"]
+
+#stop words rate per comment
+reviews["stop_words_rate"] = reviews["stop_word_count"] /reviews["punctuation"].apply(lambda x: len(x.split())) * 100 
 
 
+#Where i found the missing values and comments with just punctuation
+reviews.sort_values(by="stop_words_rate").tail(30)
+
+
+#gives an overview of how the file looks like after all transformations
+reviews.head()
+
+#removes does empty spaces
+Comments_final =reviews.dropna()
+Comments_final.sort_values(by="stop_words_rate").tail(30)
+
+#provides some statistics
+Comments_final.describe()
+
+
+#Gives a list with all the words sorted by counting
 pd.Series(" ".join(reviews["w/o_stopwords"]).split()).value_counts()
 
+
+
+#stores the words in an object
 words= " ".join(reviews["w/o_stopwords"]).split()
+
+#Getting the top x most common words
+fd= nltk.FreqDist(words)
+fd.most_common(30)
+
+### Word Cloud ------------------------------
+
+# creating the word cloud
+wc = WordCloud(background_color= "white", height = 600, width = 400)
+
+wc.generate(words)
+#adding the world cloud to file
+wc.to_file("wordcloud_output.png")
+
+
+### Sentiment analysis ----------------------
+
+#This also takes a lot of time to run and im not sure if its working properly
+
+blob = TextBlob(str(words))
+sentiment = blob.sentiment.polarity
+
+
+sentiment
+
+
+vader = SentimentIntensityAnalyzer()
+vader.polarity_scores(words)
 
 
 fd= nltk.FreqDist(words)
@@ -128,14 +190,10 @@ wc.to_file("wordcloud_output.png")
 
 #sentiment analysis
 
-blob = TextBlob(words)
-sentiment = blob.sentiment.polarity
-
-sentiment
 
 
 
-#plots
+#plots / visualization
 
 common = fd.most_common()
 
