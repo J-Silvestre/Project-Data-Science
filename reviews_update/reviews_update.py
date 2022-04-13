82be62f1f3f532fd8c6ea0ef6f91cd7e068eb329
RM-Mueller <muellerrosanna@gmail.com>
2022-04-13 09:34:27 +0100

    Reviews_update
    adjusted code for reviews update

--- reviews_update/reviews_update.py
+++ reviews_update/reviews_update.py
@@ -8,23 +8,30 @@ Created on Wed Apr  6 09:34:47 2022
 # Import libraries
 import pandas as pd  
 
-# Read splitted review files 
-df_reviews_1 = pd.read_csv("reviews_1.csv", sep=",")
-df_reviews_2 = pd.read_csv("reviews_2.csv", sep=",")
-df_reviews_3 = pd.read_csv("reviews_3.csv", sep=",")
-df_reviews_4 = pd.read_csv("reviews_4.csv", sep=",")
+# Read splitted review files -> Important: Directory needs to be changed!
+df_reviews_1 = pd.read_csv("C:\\Users\\Rosan\\Documents\\GitHub\\reviews_update\\reviews_1.csv", sep=",")
+df_reviews_2 = pd.read_csv("C:\\Users\\Rosan\\Documents\\GitHub\\reviews_update\\reviews_2.csv", sep=",")
+df_reviews_3 = pd.read_csv("C:\\Users\\Rosan\\Documents\\GitHub\\reviews_update\\reviews_3.csv", sep=",")
+df_reviews_4 = pd.read_csv("C:\\Users\\Rosan\\Documents\\GitHub\\reviews_update\\reviews_4.csv", sep=",")
 
-# Delete in each df rows with missing values
+# Delete in each df rows with missing values 
 df_reviews_1 = df_reviews_1.dropna()
 df_reviews_2 = df_reviews_2.dropna()
 df_reviews_3 = df_reviews_3.dropna()
 df_reviews_4 = df_reviews_4.dropna()
 
-# Safe each df in one file called "update"
-df_reviews_1.to_csv("reviews_update_1.csv", sep=",")
-df_reviews_2.to_csv("reviews_update_2.csv", sep=",")
-df_reviews_3.to_csv("reviews_update_3.csv", sep=",")
-df_reviews_4.to_csv("reviews_update_4.csv", sep=",")
+
+# Eliminate \r<br/>
+df_reviews_1["comments"] = df_reviews_1["comments"].str.replace('\r<br/>', '')
+df_reviews_2["comments"] = df_reviews_2["comments"].str.replace('\r<br/>', '')
+df_reviews_3["comments"] = df_reviews_3["comments"].str.replace('\r<br/>', '')
+df_reviews_4["comments"] = df_reviews_4["comments"].str.replace('\r<br/>', '')
+
+# Safe each df in one file called "update" -> Important: Directory needs to be changed!
+df_reviews_1.to_csv("C:\\Users\\Rosan\\Documents\\GitHub\\reviews_update\\reviews_update_1.csv", sep=",")
+df_reviews_2.to_csv("C:\\Users\\Rosan\\Documents\\GitHub\\reviews_update\\reviews_update_2.csv", sep=",")
+df_reviews_3.to_csv("C:\\Users\\Rosan\\Documents\\GitHub\\reviews_update\\reviews_update_3.csv", sep=",")
+df_reviews_4.to_csv("C:\\Users\\Rosan\\Documents\\GitHub\\reviews_update\\reviews_update_4.csv", sep=",")
 
 
 '''
@@ -32,14 +39,7 @@ df_reviews_4.to_csv("reviews_update_4.csv", sep=",")
 dfs = [df_reviews_1, df_reviews_2, df_reviews_3, df_reviews_4]
 merged_df = pd.concat(dfs)
 
-# Delete rows with missing values (Note: In those cases the review is missing)
+# Delete rows with missing values (Note: In those cases the comment is missing)
 merged_df = merged_df.dropna()
 '''
 
-'''
-# Remove "\r<br/>" from column "comments"
-# Note: Python gives an error, but removes the "\r<br/>" values correctly! But because of the error it is not possible to save the new file.
-comments = df_reviews["comments"]
-for i in range (len(comments)):
-    comments[i] = comments[i].replace("\r<br/>", "")
-'''
