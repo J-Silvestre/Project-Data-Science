6dc6f6428cf481c81ee7f1c28d8cb35770770e7e
RM-Mueller <muellerrosanna@gmail.com>
2022-04-13 10:22:15 +0100

    Work on Descriptive Statistics

--- descriptive statistics/descriptiveStatistics.py
+++ descriptive statistics/descriptiveStatistics.py
@@ -9,9 +9,12 @@ Created on Wed Apr 13 09:11:18 2022
 import pandas as pd
 
 
-
-# 
+# Plots for listingsUpdate------------------------------------------------------------- 
 
 listingsUpdate = pd.read_csv("C:\\Users\\Rosan\\Documents\\GitHub\\listing_update\\listing_update.csv", sep='\t')
-listingsUpdate.head()
+
+# Bar Chart: Top 10 Hosts with most accommodations
+barPlot_top10Hosts = listingsUpdate['host_id'].value_counts().head(10).plot.bar(title='Top 10 Hosts with most accommodations')
+barPlot_top10Hosts.set_xlabel("Host ID")
+barPlot_top10Hosts.set_ylabel("No. of accommodations")
 
