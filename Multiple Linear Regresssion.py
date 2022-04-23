9f64efd9fff4d871101b17260b0fefa917bb1d71
J-Silvestre <90836876+J-Silvestre@users.noreply.github.com>
2022-04-23 17:32:38 +0100

    Update Multiple Linear Regresssion.py

--- Multiple Linear Regresssion.py
+++ Multiple Linear Regresssion.py
@@ -11,6 +11,8 @@ from sklearn.model_selection import train_test_split
 from sklearn import linear_model as lm
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
 
 
 listings = pd.read_csv("listing_update.csv", sep = "\t")
@@ -20,14 +22,14 @@ main = pd.merge(listings, neighb, how="inner", on="neighbourhood_group")
 main = main[["price", "number_of_reviews", "calculated_host_listings_count", "availability_365", "host_response_rate", "nr_of_bathrooms", "bedrooms", "review_scores_rating", "coefficient_of_location", "purchase_power_pc", "crime_rate_pc", "population_density"]]
 
 
-y = np.array(main.review_scores_rating) #target variable
-x = main.drop(columns=["review_scores_rating"]) #predictors
+y = (main.price) #target variable
+x = main.drop(columns=["price"]) #predictors
 
 
 # splitting training and testing data
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.3, random_state=1)
-y_train = y_train.reshape(-1,1)
-y_test = y_test.reshape(-1,1)
+#y_train = y_train.reshape(-1,1)
+#y_test = y_test.reshape(-1,1)
 
 
 #x_test = x_test.reshape(-1,1)
@@ -46,4 +48,7 @@ MSE = mean_squared_error(y_test,y_pred)
 
 R2 = r2_score(y_test,y_pred)
 
-print(x_train)
+print(R2)
+print(model.intercept_)
+print(model.coef_)
+
